1381d08baec0f20636b624329dbccc5491b332ff
evanhays98 <evan.hays@epita.fr>
2024-03-15 17:15:20 +0100

    add

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,8 @@
  */
 const nextConfig = {
     output: 'export',
-
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
+    reactStrictMode: true,
+    basePath: '/test'
 }
 
-
-export default nextConfig;
+module.exports = nextConfig
