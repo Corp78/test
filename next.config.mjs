9889c4ac56eb24eeecbba5d392de1cc392718515
evanhays98 <evan.hays@epita.fr>
2024-03-15 18:34:14 +0100

    add image

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+
+/*
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
     basePath: '/test',
 }
+*/
+
+const nextConfig = {
+}
+
+
+
 
 export default nextConfig;
